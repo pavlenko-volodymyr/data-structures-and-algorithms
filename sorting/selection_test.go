45de477e8cff5f5992b1eea83f1d77c42b89693f
Volodymyr Pavlenko <vpavlenko@cogniance.com>
2020-05-21 23:50:04 +0300

    Update go test

--- sorting/selection_test.go
+++ sorting/selection_test.go
@@ -5,11 +5,23 @@ import (
 	"testing"
 )
 
+type Case struct {
+	given    []int
+	expected []int
+}
+
 func TestSelectionSort(t *testing.T) {
-	given := []int{3, 4, 1, 5, 6, 1, 7}
-	expected := []int{1, 1, 3, 4, 5, 6, 7}
-	SelectionSort(given)
-	if !reflect.DeepEqual(given, expected) {
-		t.Error("Failed")
+	testCases := []Case{
+		{[]int{3, 4, 1, 5, 6, 1, 7}, []int{1, 1, 3, 4, 5, 6, 7}},
+		{[]int{}, []int{}},
+		{[]int{1}, []int{1}},
+		{[]int{2, 1}, []int{1, 2}},
+	}
+
+	for _, testCase := range testCases {
+		SelectionSort(testCase.given)
+		if !reflect.DeepEqual(testCase.given, testCase.expected) {
+			t.Errorf("Expected '%v' doesn't equal to given '%v'", testCase.expected, testCase.given)
+		}
 	}
 }
