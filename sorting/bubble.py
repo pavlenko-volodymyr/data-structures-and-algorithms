44415134fb6d87d87e744cad249823f3a73c80fe
Volodymyr Pavlenko <vpavlenko@cogniance.com>
2020-05-21 11:31:27 +0300

    Add bubble sort in Python

---
+++ sorting/bubble.py
@@ -0,0 +1,10 @@
+def bubble_sort(arr):
+    for i in range(len(arr)):
+        for j in range(0, len(arr) - i - 1):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+
+
+arr = [3, 4, 1, 5, 6, 1, 7]
+bubble_sort(arr)
+print(arr)
