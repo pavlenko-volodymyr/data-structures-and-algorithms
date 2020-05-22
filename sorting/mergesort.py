c2e69dc77df09d6e58416112e17cb4e528c7e278
Volodymyr Pavlenko <vpavlenko@cogniance.com>
2020-05-22 21:10:10 +0300

    Merge sort in python

---
+++ sorting/mergesort.py
@@ -0,0 +1,32 @@
+def merge(left, right):
+    result = []
+    i = 0
+    j = 0
+    while i < len(left) and j < len(right):
+        if left[i] < right[j]:
+            result.append(left[i])
+            i += 1
+        elif left[i] > right[j]:
+            result.append(right[j])
+            j += 1
+        else:
+            result.append(left[i])
+            result.append(right[j])
+            i += 1
+            j += 1
+    
+    if i < len(left):
+        result.extend(left[i:])
+
+    if j < len(right):
+        result.extend(right[j:])
+
+    return result
+
+
+def merge_sort(arr: list) -> list:
+    if len(arr) < 2:
+        return arr
+
+    mid = len(arr) // 2
+    return merge(merge_sort(arr[:mid]), merge_sort(arr[mid:]))
---
+++ sorting/mergesort_test.py
@@ -0,0 +1,16 @@
+from sorting.mergesort import merge, merge_sort
+
+
+def test_merge():
+    left = [3, 4, 5]
+    right = [5, 8, 12]
+    expected = [3, 4, 5, 5, 8, 12]
+    given = merge(left, right)
+    assert given == expected
+
+
+def test_mergesort():
+    arr = [3, 2, 6, 1, 4, 6, 100, -1]
+    given = merge_sort(arr)
+    expected = [-1, 1, 2, 3, 4, 6, 6, 100]
+    assert given == expected
