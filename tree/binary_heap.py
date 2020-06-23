59bd69633e67d647fe15546498634fce5ee3b4f3
Volodymyr Pavlenko <vpavlenko@cogniance.com>
2020-06-23 10:44:44 +0300

    Fix max binary heap insert. Implement extract max.

--- tree/binary_heap.py
+++ tree/binary_heap.py
@@ -6,9 +6,48 @@ class MaxBinaryHeap:
         self.values.append(value)
         index = len(self.values) - 1
         parent_index = (index - 1) // 2
-        while self.values[parent_index] < self.values[index]:
+        while self.values[parent_index] < self.values[index] and parent_index >= 0:
             self.values[parent_index], self.values[index] = self.values[index], self.values[parent_index]
             index = parent_index
-    
+            parent_index = (index - 1) // 2
+
     def extract_max(self):
-        return self.values[0]
+        if not self.values:
+            return
+        
+        value = self.values[0]
+        self.values[0] = self.values[-1]
+        self.values.pop()
+        if self.values:
+            self.__bubble_down(0)
+
+        return value
+
+    def __bubble_down(self, index):
+        total_values = len(self.values)
+        
+        while True:
+            element = self.values[index]
+            left_child_index = 2 * index + 1
+            right_child_index = left_child_index + 1
+            swap_index = None
+
+            if left_child_index < total_values and element < self.values[left_child_index]:
+                swap_index = left_child_index
+            
+            if (
+                right_child_index < total_values and
+                element < self.values[left_child_index] and
+                self.values[right_child_index] > self.values[left_child_index]
+            ):
+                swap_index = right_child_index
+
+            if swap_index is None:
+                break
+
+            self.values[index] = self.values[swap_index]
+            self.values[swap_index] = element
+            index = swap_index
+
+    def top(self):
+        return self.values[0] if self.values else None
--- tree/binary_heap_test.py
+++ tree/binary_heap_test.py
@@ -5,10 +5,34 @@ def test_max_binary_heap():
     max_binary_heap = MaxBinaryHeap()
 
     max_binary_heap.insert(10)
-    assert max_binary_heap.extract_max() == 10
+    assert max_binary_heap.top() == 10
 
     max_binary_heap.insert(30)
-    assert max_binary_heap.extract_max() == 30
+    assert max_binary_heap.top() == 30
 
     max_binary_heap.insert(1)
-    assert max_binary_heap.extract_max() == 30
+    assert max_binary_heap.top() == 30
+
+    max_binary_heap.insert(50)
+    assert max_binary_heap.top() == 50
+
+    max_binary_heap.insert(100)
+    assert max_binary_heap.top() == 100
+
+    value = max_binary_heap.extract_max()
+    assert value == 100
+
+    value = max_binary_heap.extract_max()
+    assert value == 50
+
+    value = max_binary_heap.extract_max()
+    assert value == 30
+
+    value = max_binary_heap.extract_max()
+    assert value == 10
+
+    value = max_binary_heap.extract_max()
+    assert value == 1
+
+    value = max_binary_heap.extract_max()
+    assert value == None
